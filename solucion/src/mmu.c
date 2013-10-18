61dbc36443420aff81dd4b15f62fa8e00c3c47cf
Alvaro Bruno Machicado Macias <amacias@ws16.labo5>
2013-10-17 21:33:15 -0300

    mapeando direcciones para tareas

--- solucion/src/mmu.c
+++ solucion/src/mmu.c
@@ -29,9 +29,31 @@ void mmu_inicializar() {
 }
 
 void mmu_inicializar_dir_tarea(int nro_tarea) {
-	
+
 	int* dir_fisica = ((nro_tarea -1) * TASK_SIZE) + AREA_MAR_INICIO;
+	int* dir_address = TASK_1_PAGE_DIR+ ((nro_tarea-1) <<12);
+	int* table_address = TASK_1_PAGE_TABLE_1 + (((nro_tarea-1)*2) <<12);
 	
+	for (i=0; i< 1024 ;i++) {
+		dir_address[i] = 0;
+	}
+	
+	dir_address[0] = table_address;
+	dir_address[1] = table_address + (1<<12);
+	
+	for (i=0; i<0x780 ; i++) {
+		table_address[i] = (i<<12)+3;
+	}
+	for (i=0x780; i<2048 ; i++){
+		table_address[i]=0;
+	}
+		
+}
+
+void mmu_mapear_pagina (unsigned int virtual, unsigned int cr3, unsigned int fisica, unsigned int attr){
+	int dir_index = virtual
 	
 	
 }
+
+
