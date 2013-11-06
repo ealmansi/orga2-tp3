99573d8b0de8ced727235e4783cf56ad30931e81
Miguel Duarte <miguelfeliped@gmail.com>
2013-11-06 19:49:36 -0300

    Commit al pedo.

--- solucion/src/defines.h
+++ solucion/src/defines.h
@@ -64,6 +64,13 @@
 #define TASK_8_PAGE_DIR 		0x00037000 /* direccion fisica para el directorio de paginas de la tarea 8 */
 
 #define TASK_1_PAGE_TABLE		0x00038000 /* direccion fisica para la tabla de paginas de la tarea 1 */
+#define TASK_2_PAGE_TABLE		0x00039000 /* direccion fisica para la tabla de paginas de la tarea 2 */
+#define TASK_3_PAGE_TABLE		0x0003A000 /* direccion fisica para la tabla de paginas de la tarea 3 */
+#define TASK_4_PAGE_TABLE		0x0003B000 /* direccion fisica para la tabla de paginas de la tarea 4 */
+#define TASK_5_PAGE_TABLE		0x0003C000 /* direccion fisica para la tabla de paginas de la tarea 5 */
+#define TASK_6_PAGE_TABLE		0x0003D000 /* direccion fisica para la tabla de paginas de la tarea 6 */
+#define TASK_7_PAGE_TABLE		0x0003E000 /* direccion fisica para la tabla de paginas de la tarea 7 */
+#define TASK_8_PAGE_TABLE		0x0003F000 /* direccion fisica para la tabla de paginas de la tarea 8 */
 
 
 #define TASK_1_STACK_RING_0 	0x00040000 /* direccion fisica para la pila de nivel 0 de la tarea 1 */
