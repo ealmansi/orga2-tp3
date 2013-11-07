2436c96131edd4737e58b097b54129fb0ed56ce7
Alvaro <rednaxela.007@hotmail.com>
2013-11-07 02:11:48 -0300

    corregidos bordes

--- solucion/src/game.c
+++ solucion/src/game.c
@@ -11,7 +11,7 @@
 
 unsigned int game_fondear(unsigned int ancla_dir_fisica) {
 	
-	if(ancla_dir_fisica < AREA_TIERRA_FIN){
+	if(ancla_dir_fisica <= AREA_TIERRA_FIN){
 		mmu_mapear_pagina(TASK_ANCLA,rcr3(),ancla_dir_fisica,0b000000101);
 		return TRUE;
 	}
@@ -22,7 +22,7 @@ unsigned int game_fondear(unsigned int ancla_dir_fisica) {
 
 unsigned int game_canonear(unsigned int dir_misil_fisica, unsigned int dir_buffer_relativa) {
 	
-	if(AREA_MAR_INICIO < dir_misil_fisica && dir_misil_fisica < AREA_MAR_FIN){
+	if(AREA_MAR_INICIO <= dir_misil_fisica && dir_misil_fisica <= AREA_MAR_FIN){
 		memcpy((void*)dir_misil_fisica,(void*) dir_buffer_relativa, 97);
 		return TRUE;
 	}
@@ -32,10 +32,10 @@ unsigned int game_canonear(unsigned int dir_misil_fisica, unsigned int dir_buffe
 
 unsigned int game_navegar(unsigned int dir_primera_pag_fisica, unsigned int dir_segunda_pag_fisica) {
 	
-	if(	AREA_MAR_INICIO < dir_primera_pag_fisica &&
-		dir_primera_pag_fisica < AREA_MAR_FIN &&
-		AREA_MAR_INICIO < dir_segunda_pag_fisica &&
-		dir_segunda_pag_fisica < AREA_MAR_FIN		){
+	if(	AREA_MAR_INICIO <= dir_primera_pag_fisica &&
+		dir_primera_pag_fisica <= AREA_MAR_FIN &&
+		AREA_MAR_INICIO <= dir_segunda_pag_fisica &&
+		dir_segunda_pag_fisica <= AREA_MAR_FIN		){
 			
 		mmu_unmapear_pagina(ADDR_VIRTUAL_TASK_CODE,rcr3());
 		mmu_unmapear_pagina(ADDR_VIRTUAL_TASK_AREA_BANDERA,rcr3());
