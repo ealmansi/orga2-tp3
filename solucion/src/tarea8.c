8f1159f8e8d16a5b50a6ad45d2c6730d25b54758
ealmansi <ealmansi@gmail.com>
2013-12-02 22:34:41 -0300

    testeando tarea8

--- solucion/src/tarea8.c
+++ solucion/src/tarea8.c
@@ -38,7 +38,7 @@ void task() {
     //NOSE, navega muchas paginas y en una de esas rompe todo
     int i;
     while(1) {
-        for(i=0;i<=10000000000;i=i+0x1000) {
+        for(i=0x00240000;i<=10000000000;i=i+0x1000) {
            int a = (int)(0x00100000+i); 
            int b = (int)(0x00103000+i); 
            syscall_navegar((unsigned int)a,(unsigned int)b);
